dc70d36ff1db9e6a0e2e83764e780327007c1801
Guilherme Oliveira <sirguilhermeoliveira@gmail.com>
2024-06-04 00:03:35 -0300

    bugfix: correction global import

--- src/app/routes/app.routes.ts
+++ src/app/routes/app.routes.ts
@@ -1,6 +1,6 @@
 import { Routes, RouterModule  } from '@angular/router';
 import { LoginComponent } from '../pages/login/login.component';
-import { HomeComponent } from '../pages/home/home.component';
+import { HomeComponent } from '@pages/home/home.component';
 
 
 export const routes: Routes = [  { path: '', redirectTo: '/login', pathMatch: 'full' },
