ade37a65460ac67ecdda84b8c2b735d3fbe71074
Guilherme Oliveira <sirguilhermeoliveira@gmail.com>
2024-06-04 20:20:45 -0300

    feat: form validation and mask

--- src/app/core/app.config.ts
+++ src/app/core/app.config.ts
@@ -3,7 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from '../routes/app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { IConfig, provideEnvironmentNgxMask } from 'ngx-mask'
+
+const maskConfig: Partial<IConfig> = {
+  validation: false,
+};
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideEnvironmentNgxMask(maskConfig), provideRouter(routes), provideClientHydration()]
 };
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,26 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { RowComponent } from '@components/global/row/row.component';
+import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-loging',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, RowComponent, NgxMaskDirective, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
+
 export class LoginComponent {
   title = 'Login';
-  username: string = '';
-  password: string = '';
+  cellphone: string | undefined;
+  password: string | undefined;
+  passwordFieldType: string = 'password';
   authentication: boolean = true;
   
   constructor(private router: Router) {}
 
+  togglePasswordVisibility() {
+    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  }
+
   login() {
-    console.log(this.username);
-    console.log(this.password);
-    this.username = '';
+    this.cellphone = '';
     this.password = '';
     this.authentication = true;
     if(this.authentication){
