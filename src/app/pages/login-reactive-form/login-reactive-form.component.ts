879c55b14d9f9c77a8d4fe0e8875ffe1a87f3ffa
Guilherme Oliveira <sirguilhermeoliveira@gmail.com>
2024-06-05 07:29:13 -0300

    feat: reactive form

--- src/app/pages/login-reactive-form/login-reactive-form.component.ts
+++ src/app/pages/login-reactive-form/login-reactive-form.component.ts
@@ -1,38 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms'; // Importe FormGroup, FormBuilder e Validators
 import { Router } from '@angular/router';
-import { RowComponent } from '@components/global/row/row.component';
 import { NgxMaskDirective } from 'ngx-mask';
 
 @Component({
   selector: 'app-login-reactive-form',
   standalone: true,
-  imports: [FormsModule, RowComponent, NgxMaskDirective, CommonModule],
   templateUrl: './login-reactive-form.component.html',
-  styleUrl: './login-reactive-form.component.css'
+  styleUrls: ['./login-reactive-form.component.css'],
+  imports: [CommonModule, ReactiveFormsModule, NgxMaskDirective],
 })
-
-
 export class LoginReactiveFormComponent {
-  title = 'Login';
-  cellphone: string | undefined;
-  password: string | undefined;
+  loginForm: FormGroup; // Declare um FormGroup
   passwordFieldType: string = 'password';
   authentication: boolean = true;
-  
-  constructor(private router: Router) {}
 
-  togglePasswordVisibility() {
-    this.passwordFieldType = this.passwordFieldType === 'password' ? 'text' : 'password';
+  constructor(private fb: FormBuilder, private router: Router) { 
+    this.loginForm = this.fb.group({ 
+      cellphone: ['', [Validators.required, Validators.minLength(11), Validators.maxLength(15)]], 
+      password: ['', [Validators.required, Validators.minLength(6)]] 
+    });
   }
 
   login() {
-    this.cellphone = '';
-    this.password = '';
+    const cellphone = this.loginForm.get('cellphone')?.value;
+    const password = this.loginForm.get('password')?.value;
     this.authentication = true;
-    if(this.authentication){
+    if(this.authentication && cellphone.length > 0 && password.length > 0) { 
         this.router.navigate(['/home']);
     }
   }
-}
+  }
